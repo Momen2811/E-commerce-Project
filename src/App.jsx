09233a70b34f8002a2b6a1a745fdce0cbd9187ef
momen <momenmoussa7@gmail.com>
2026-05-29 03:26:45 +0300

    feat: mount AuthProvider and auth routes (account protected)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import { WishlistProvider } from './context/WishlistContext.jsx'
 import { UIProvider } from './context/UIContext.jsx'
@@ -6,6 +7,7 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import CartDrawer from './components/CartDrawer.jsx'
 import QuickViewModal from './components/QuickViewModal.jsx'
+import RequireAuth from './components/RequireAuth.jsx'
 import HomePage from './pages/HomePage.jsx'
 import ShopPage from './pages/ShopPage.jsx'
 import ProductPage from './pages/ProductPage.jsx'
@@ -14,35 +16,45 @@ import WishlistPage from './pages/WishlistPage.jsx'
 import SearchPage from './pages/SearchPage.jsx'
 import CheckoutPage from './pages/CheckoutPage.jsx'
 import OrderConfirmationPage from './pages/OrderConfirmationPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
+import ForgotPasswordPage from './pages/ForgotPasswordPage.jsx'
+import AccountPage from './pages/AccountPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 
 export default function App() {
   return (
-    <CartProvider>
-      <WishlistProvider>
-        <UIProvider>
-          <div className="app">
-            <Header />
-            <main className="main">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/shop" element={<ShopPage />} />
-                <Route path="/shop/:audience" element={<ShopPage />} />
-                <Route path="/product/:slug" element={<ProductPage />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/wishlist" element={<WishlistPage />} />
-                <Route path="/search" element={<SearchPage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route path="/order-confirmation" element={<OrderConfirmationPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </main>
-            <Footer />
-            <CartDrawer />
-            <QuickViewModal />
-          </div>
-        </UIProvider>
-      </WishlistProvider>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <WishlistProvider>
+          <UIProvider>
+            <div className="app">
+              <Header />
+              <main className="main">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/shop" element={<ShopPage />} />
+                  <Route path="/shop/:audience" element={<ShopPage />} />
+                  <Route path="/product/:slug" element={<ProductPage />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route path="/wishlist" element={<WishlistPage />} />
+                  <Route path="/search" element={<SearchPage />} />
+                  <Route path="/checkout" element={<CheckoutPage />} />
+                  <Route path="/order-confirmation" element={<OrderConfirmationPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegisterPage />} />
+                  <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+                  <Route path="/account" element={<RequireAuth><AccountPage /></RequireAuth>} />
+                  <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+              </main>
+              <Footer />
+              <CartDrawer />
+              <QuickViewModal />
+            </div>
+          </UIProvider>
+        </WishlistProvider>
+      </CartProvider>
+    </AuthProvider>
   )
 }
